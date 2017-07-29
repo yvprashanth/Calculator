b1cecd9f924d796ff9ac927fe032c7033962f220
Prashanth <yvprashanth@gmail.com>
2017-07-29 11:52:02 -0700

    got basic button set working

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,25 +1,44 @@
-//
 //  ViewController.swift
 //  Calculator
 //
-//  Created by PrashMaya2 on 7/27/17.
+//  Created by PrashMaya on 7/27/17.
 //  Copyright © 2017 Prashanth Yerramilli. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display!.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                display!.text = String(Double.pi)
+            case "√":
+                let operand = Double(display!.text!)
+                display!.text = String(sqrt(operand!))
+            default:
+                break
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func resetDisplay(_ sender: UIButton) {
+        display!.text = "0"
     }
-
-
 }
-
