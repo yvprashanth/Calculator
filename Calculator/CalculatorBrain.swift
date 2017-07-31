04e4b4a1875fd7d0b5fd5b6ed35156c3a51b4209
Prashanth Yerramilli <yvprashanth@gmail.com>
2017-07-30 19:40:53 -0700

    Main (#1)
    * associated value
    
    * got the binaryOperation to work as well
    
    * + works. The rest does not
    
    * some more
    
    * added cos

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,21 +8,72 @@
 
 import Foundation
 
+func multiply(input1: Double, input2: Double) -> Double{
+    return input1 * input2
+}
+
 struct CalculatorBrain {
-    
     // Accumulated Result
     private var accumulator : Double?
     
-    mutating func performOperation(_ mathematicalSymbol: String){
-        switch mathematicalSymbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations : Dictionary<String, Operation> =
+        [
+            "π" : Operation.constant(Double.pi),
+            "e": Operation.constant(M_E),
+            "cos" : Operation.unaryOperation(cos),
+            "√" : Operation.unaryOperation(sqrt),
+            "×" : Operation.binaryOperation({$0 * $1}),
+            "+" : Operation.binaryOperation({$0 + $1}),
+            "÷" : Operation.binaryOperation({$0 / $1}),
+            "-" : Operation.binaryOperation({$0 - $1}),
+            "=" : Operation.equals
+        ]
+    
+    mutating func performOperation(_ symbol: String){
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil{
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+                break
+            case .equals:
+                performPendingBinaryOperation()
             }
-        default:
-            break
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        // With an ! it will unwrap it, but if it's just a question mark then if it's set then it will unwrap, else it will just ignore it
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation?.perform(with: accumulator!)
+            // No longer are we in the middle of pending binary operation
+            pendingBinaryOperation = nil
+        }
+    }
+            
+    private var pendingBinaryOperation : PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let function : (Double, Double) -> Double
+        let firstOperand : Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
         }
     }
     
